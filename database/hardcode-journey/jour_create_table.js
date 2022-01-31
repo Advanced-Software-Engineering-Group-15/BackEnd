f05416e412b2d20db63618fb6c23841cb01a8c0b
Claudia Alonso <alonsovc@tcd.ie>
2022-01-31 15:42:48 +0000

    hardcode-journey: Change to columns

--- database/hardcode-journey/jour_create_table.js
+++ database/hardcode-journey/jour_create_table.js
@@ -12,14 +12,24 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE journeyList (\
-        id INT AUTO_INCREMENT PRIMARY KEY, creator_rating FLOAT, creator_name VARCHAR(255), creator_id INT,\
-         journey_type VARCHAR(255), cost FLOAT, spots_left INT,\
-         date_of_journey DATETIME, \
-         start_latitude DECIMAL(8,6), start_longitude DECIMAL(9,6),\
-         end_latitude DECIMAL(8,6), end_longitude DECIMAL(9,6))"; // https://stackoverflow.com/questions/1196415/what-datatype-to-use-when-storing-latitude-and-longitude-data-in-sql-databases
+    var sql = "ALTER TABLE journeyList ADD COLUMN start_coords POINT NOT NULL, ADD COLUMN end_coords POINT NOT NULL";
     con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log("Table created");
+      console.log("Table altered");
     });
   });
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "CREATE TABLE journeyList (\
+//         id INT AUTO_INCREMENT PRIMARY KEY, creator_rating FLOAT, creator_name VARCHAR(255), creator_id INT,\
+//          journey_type VARCHAR(255), cost FLOAT, spots_left INT,\
+//          date_of_journey DATETIME, \
+//          start_latitude DECIMAL(8,6), start_longitude DECIMAL(9,6),\
+//          end_latitude DECIMAL(8,6), end_longitude DECIMAL(9,6))"; // https://stackoverflow.com/questions/1196415/what-datatype-to-use-when-storing-latitude-and-longitude-data-in-sql-databases
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//     });
+//   });
