89f2a236351aafc6af4fd8e309655e03db24532f
Conor Fennell <fennelco@tcd.ie>
2022-02-17 15:33:20 +0000

    loginverify: created query methods for database

--- app.tsx
+++ app.tsx
@@ -1,3 +1,5 @@
+import { resolve } from "path/posix";
+
 const dataBaseHelper = require('./database/db_checkCredentials')
 
 const express = require('express')
@@ -7,21 +9,28 @@ const port = 5000
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 app.get('/journeys', (req, res) => {
   const exJourneys = require('./exJourneys.json');
   console.log(exJourneys)
   res.json(exJourneys)
 })
 
+
 app.post('/sign-in', (req, res) => {
   const data = JSON.parse(req.body.body)
   console.log(data);
   const db = new dataBaseHelper(data.userName, data.password)
-  const loginStatus = db.isValidCreds;
-  console.log("Login status sent to frontend is: ", loginStatus);
-  res.json(req.body); //not actual response value yet
+
+  const loginStatus = new Promise((resolve, reject) => {
+    try{resolve(db.isValidCreds)}
+    catch{reject(false)}
+  }).then((status) => {
+    console.log("Login status sent to frontend is: ", status);
+    const response = {
+      isLoginSuccessful: status
+    }
+    res.json(response); 
+  });
 })
 
 app.listen(port, () => {
--- database/db_checkCredentials.js
+++ database/db_checkCredentials.js
@@ -1,44 +1,45 @@
 class dataBaseHelper {
 
     constructor(userName, password){
-        this.userName = userName;
-        this.password = password;
+        this.userName = userName.toString();
+        this.password = password.toString();
         this.mysql = require('mysql');
-        this.sql = "SELECT * FROM userAccountInfo WHERE username = ?";
-        this.con = this.mysql.createConnection({
+    }
+
+    //returns true or false if credentials are in database
+    get isValidCreds() {
+        var sql = "SELECT * FROM userAccountInfo WHERE username = ?";
+        console.log("Querying database: Username: ", this.userName, ", Password: ", this.password);
+        const con = this.mysql.createConnection({
             host: "user-information-database.cl7ouywfgywl.eu-west-1.rds.amazonaws.com",
             port: 3306,
             user: "masterUsername",
             password: "password",
             database: "User_Information_Database"
         });
-    }
 
-    //returns true or false if credentials are in database
-    get isValidCreds() {
-        console.log("Querying database...");
-        //return this.queryDatabase;
-        this.con.connect(this.querydb());
-    }
+        var user = {
+            userName: this.userName,
+            password: this.password
+        };
 
-    querydb = function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        //bug here, possibly need to download mysql on machine, possible coding issue
-        this.con.query(this.sql, [this.userName], function (err, result) {
-            if (err) {
-                //This is is username is not found in Database 
-                console.log("This username is not present: " + this.userName);
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+            con.query(sql, [user.userName], function (err, result) {
+                if (err) {
+                    //This is is username is not found in Database 
+                    console.log("This username is not present: ", user.userName);
+                    return false;
+                }
+                // If Username found in Database
+                console.log("Found User: ", user.userName);
+                if (result[0].password == user.password){
+                    console.log("Successful password")
+                    return true;
+                }
                 return false;
-            }
-            // If Username found in Database
-            console.log("Found User: " + this.userName);
-            console.log(result[0].password) //Need the zero to access the column names like password
-            if (result[0].password == this.password){
-                console.log("Successful password")
-                return true;
-            }
-            return false;
+            });
         });
     }
 }
