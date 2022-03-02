9accaa5707c9ee40c62eeb61eba86505d93610e4
conor fennell <fennelco@tcd.ie>
2022-03-02 16:47:57 +0000

    wk6: fixed bugs in merge

--- app.tsx
+++ app.tsx
@@ -38,74 +38,6 @@ app.get('/createdJourneys', (req, res) => {
   res.json(newJourney)
 });
 
-<<<<<<< HEAD
-=======
-app.post('/newJourneys', (req, res) =>{
-  console.log(req.body.body)
-  res.json(req.body)
-  // res.end('Success')
-  const exJourneys = require('./exJourneys.json');
-  console.log(exJourneys)
-  const data = JSON.parse(req.body.body)
-  exJourneys.exJourneys.push(data)
-  console.log(data)
-  console.log(exJourneys)
-  // fileSystem.writeFile("./newJourney.json", JSON.stringify(data), err=>{
-  //   if(err){
-  //     console.log("Error writing file" ,err)
-  //   } else {
-  //     console.log('JSON data is written to the file successfully')
-  //   }
-  // })
-
-  /// get all records from mySQL table
-  var sql = "INSERT INTO journeyListFormat (journeyID, journeyType, startName, startLat, startLong, endName, endLat, endLong, currency, cost, creatorID, creatorRating) VALUES ?";
-  //const exJourneys = require('../exJourneys.json');
-  //console.log(exJourneys)
-  //console.log(exJourneys.exJourneys.length)
-  var values = []
-  console.log("data: ")
-  console.log(data)
-  var currJourney = data;
-  console.log("currJourney: ")
-  console.log(currJourney)
-  var currValues = [
-    currJourney.journeyID,
-    currJourney.journeyType,
-    currJourney.journeyStart.name,
-    currJourney.journeyStart.latitude,
-    currJourney.journeyStart.longitude,
-    currJourney.journeyEnd.name,
-    currJourney.journeyEnd.latitude,
-    currJourney.journeyEnd.longitude,
-    currJourney.pricing.currency,
-    currJourney.pricing.quantity,
-    currJourney.creatorID,
-    currJourney.creatorRating
-  ];
-  console.log("currValues: ")
-  console.log(currValues)
-  values[0] = currValues;
-  console.log(values)
-  // var values = [
-  //   ['tester', 'Tester', 'tester@test.com', 'password', 4.0 ],
-  //   ['tester_2','Tester_2', 'secondTester@test.com', 'password2', 3.5 ],
-  //   ['Vanilla_Dreams', 'Vanilla', 'vanillaDreams@dream.com', 'password3', 4.8]
-  // ];
-  con.query(sql, [values], function (err, result) {
-    if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
-  fileSystem.writeFile("./exJourneys.json", JSON.stringify(exJourneys, null, 2), err=>{
-    if(err){
-      console.log("Error writing file" ,err)
-    } else {
-      console.log('JSON data is written to exJourneys file successfully')
-    }
-  })
-});
-
->>>>>>> origin/master
 app.post('/sign-in', (req, res) => {
   const data = JSON.parse(req.body.body)
   console.log(data);
@@ -138,7 +70,6 @@ app.post('/new-user', (req, res) => {
 });
 
 app.listen(port, () => {
-<<<<<<< HEAD
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
@@ -169,8 +100,3 @@ db.isValidCreds().then(() => {
   console.log('response sent to server is: ', response);
 })
 */
-=======
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
->>>>>>> origin/master
