9c2d08f032413481a0f86fa3928e8837f25f4d99
unknown <lynchm18@tcd.ie>
2022-03-02 15:31:22 +0000

    fixed double create error

--- app.tsx
+++ app.tsx
@@ -13,6 +13,11 @@ var con = mysql.createConnection({
   database: "User_Information_Database"
 });
 
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -45,52 +50,50 @@ app.post('/newJourneys', (req, res) =>{
   //     console.log('JSON data is written to the file successfully')
   //   }
   // })
-  con.connect(function(err) {
+
+  /// get all records from mySQL table
+  var sql = "INSERT INTO journeyListFormat (journeyID, journeyType, startName, startLat, startLong, endName, endLat, endLong, currency, cost, creatorID, creatorRating) VALUES ?";
+  //const exJourneys = require('../exJourneys.json');
+  //console.log(exJourneys)
+  //console.log(exJourneys.exJourneys.length)
+  var values = []
+  console.log("data: ")
+  console.log(data)
+  var currJourney = data;
+  console.log("currJourney: ")
+  console.log(currJourney)
+  var currValues = [
+    currJourney.journeyID,
+    currJourney.journeyType,
+    currJourney.journeyStart.name,
+    currJourney.journeyStart.latitude,
+    currJourney.journeyStart.longitude,
+    currJourney.journeyEnd.name,
+    currJourney.journeyEnd.latitude,
+    currJourney.journeyEnd.longitude,
+    currJourney.pricing.currency,
+    currJourney.pricing.quantity,
+    currJourney.creatorID,
+    currJourney.creatorRating
+  ];
+  console.log("currValues: ")
+  console.log(currValues)
+  values[0] = currValues;
+  console.log(values)
+  // var values = [
+  //   ['tester', 'Tester', 'tester@test.com', 'password', 4.0 ],
+  //   ['tester_2','Tester_2', 'secondTester@test.com', 'password2', 3.5 ],
+  //   ['Vanilla_Dreams', 'Vanilla', 'vanillaDreams@dream.com', 'password3', 4.8]
+  // ];
+  con.query(sql, [values], function (err, result) {
     if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO journeyListFormat (journeyID, journeyType, startName, startLat, startLong, endName, endLat, endLong, currency, cost, creatorID, creatorRating) VALUES ?";
-    //const exJourneys = require('../exJourneys.json');
-    //console.log(exJourneys)
-    //console.log(exJourneys.exJourneys.length)
-    var values = []
-      console.log("data: ")
-      console.log(data)
-      var currJourney = data;
-      console.log("currJourney: ")
-      console.log(currJourney)
-      var currValues = [
-        currJourney.journeyID,
-        currJourney.journeyType,
-        currJourney.journeyStart.name,
-        currJourney.journeyStart.latitude,
-        currJourney.journeyStart.longitude,
-        currJourney.journeyEnd.name,
-        currJourney.journeyEnd.latitude,
-        currJourney.journeyEnd.longitude,
-        currJourney.pricing.currency,
-        currJourney.pricing.quantity,
-        currJourney.creatorID,
-        currJourney.creatorRating
-      ];
-      console.log("currValues: ")
-      console.log(currValues)
-    values[0] = currValues;
-    console.log(values)
-    // var values = [
-    //   ['tester', 'Tester', 'tester@test.com', 'password', 4.0 ],
-    //   ['tester_2','Tester_2', 'secondTester@test.com', 'password2', 3.5 ],
-    //   ['Vanilla_Dreams', 'Vanilla', 'vanillaDreams@dream.com', 'password3', 4.8]
-    // ];
-    con.query(sql, [values], function (err, result) {
-      if (err) throw err;
-      console.log("Number of records inserted: " + result.affectedRows);
-    });
+    console.log("Number of records inserted: " + result.affectedRows);
   });
   fileSystem.writeFile("./exJourneys.json", JSON.stringify(exJourneys, null, 2), err=>{
     if(err){
       console.log("Error writing file" ,err)
     } else {
-      console.log('JSON data is written to newJourneys file successfully')
+      console.log('JSON data is written to exJourneys file successfully')
     }
   })
 });
